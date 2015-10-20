28709541a4f390ba4fe6545e0b62679c0c07db16
lubrige <lubrige2@gmail.com>
2015-10-20 10:31:19 +0800

    modify icpc 6047 to use rabin karp

--- icpc/6047.cpp
+++ icpc/6047.cpp
@@ -1,5 +1,3 @@
-// 6047. Perfect Matching
-
 #include <bits/stdc++.h>
 #define sz(x) int((x).size())
 #define all(x) (x).begin(), (x).end()
@@ -12,37 +10,34 @@
 using namespace std;
 
 typedef long long ll;
+const ll kQ = 3037000399LL;
+const ll kP = 37;
 const int kN = 1010;
 const int kL = 510;
-
-struct TNode {
-	int ch[26];
-	bool has;
-
-	void clear() {
-		fill(&ch[0], &ch[26], -1), has = false;
-	}
-} tree1[kN * kL], tree2[kN * kL];
 
 int Z, N;
 string S[kN];
 
-int tr_cnt1, tr_cnt2;
-int f[2 * kL];
+ll pw[kL];
 
+void prep();
 void read();
 void solve(int);
 
-void ins_str(const string &, TNode [], int &);
-void manacher(const string &);
-
 int main() {
 	ios::sync_with_stdio(false);
+	prep();
 	cin >> Z;
 	for (int zi = 1; zi <= Z; ++zi)
 		read(), solve(zi);
 
 	return 0;
+}
+
+void prep() {
+	pw[0] = 1;
+	for (int i = 1; i < kL; ++i)
+		pw[i] = pw[i - 1] * kP % kQ;
 }
 
 void read() {
@@ -52,75 +47,25 @@ void read() {
 }
 
 void solve(int zi) {
-	tree1[0].clear(), tree2[0].clear(), tr_cnt1 = tr_cnt2 = 1;
+	static ll f1[kN], f2[kN];
+
 	for (int i = 0; i < N; ++i) {
-		reverse(all(S[i]));
-		ins_str(S[i], tree1, tr_cnt1);
-		reverse(all(S[i]));
-		ins_str(S[i], tree2, tr_cnt2);
+		f1[i] = f2[i] = 0;
+		for (int j = 0; j < sz(S[i]); ++j)
+			f1[i] = (f1[i] * kP % kQ + ll(S[i][j] - 'a') + 1) % kQ;
+		for (int j = sz(S[i]) - 1; j >= 0; --j)
+			f2[i] = (f2[i] * kP % kQ + ll(S[i][j] - 'a') + 1) % kQ;
 	}
 
-	int res = 0, u;
-	for (int i = 0; i < N; ++i) {
-		manacher(S[i]);
-
-		u = 0;
-		for (int j = 0; j < sz(S[i]); ++j) {
-			int ci = int(S[i][j] - 'a');
-			if (tree1[u].ch[ci] == -1)
-				break;
-			u = tree1[u].ch[ci];
-			if (tree1[u].has && f[sz(S[i]) + j + 2] >= (sz(S[i]) - j - 1))
+	int res = 0;
+	for (int i = 0; i < N; ++i)
+		for (int j = i + 1; j < N; ++j) {
+			int si = sz(S[i]), sj = sz(S[j]);
+			if ((f1[i] * pw[sj] % kQ + f1[j]) % kQ == (f2[j] * pw[si] % kQ + f2[i]) % kQ)
+				++res;
+			if ((f1[j] * pw[si] % kQ + f1[i]) % kQ == (f2[i] * pw[sj] % kQ + f2[j]) % kQ)
 				++res;
 		}
-
-		u = 0;
-		for (int j = 0; j < sz(S[i]) - 1; ++j) {
-			int ci = int(S[i][sz(S[i]) - j - 1] - 'a');
-			if (tree2[u].ch[ci] == -1)
-				break;
-			u = tree2[u].ch[ci];
-			if (tree2[u].has && f[sz(S[i]) - j] >= (sz(S[i]) - j - 1))
-				++res;
-		}
-
-		// palindrome concatenating itself will be counted, exclude it
-		if (f[sz(S[i]) + 1] >= sz(S[i]))
-			--res;
-	}
 
 	cout << "Case #" << zi << ": " << res << "\n";
-}
-
-void ins_str(const string &t, TNode tree[], int &tr_cnt) {
-	int u = 0;
-	for (int i = 0; i < sz(t); ++i) {
-		int ci = int(t[i] - 'a');
-		if (tree[u].ch[ci] == -1)
-			tree[tr_cnt].clear(), tree[u].ch[ci] = tr_cnt, ++tr_cnt;
-		u = tree[u].ch[ci];
-	}
-	tree[u].has = true;
-}
-
-void manacher(const string &s) {
-	string t = "^";
-	int c, r;
-
-	for (int i = 0; i < sz(s); ++i)
-		t += "#", t += s[i];
-	t += "#$";
-
-	fill(&f[0], &f[2 * sz(s) + 3], 0);
-	f[0] = c = r = 0;
-	for (int i = 1; i < sz(t) - 1; ++i) {
-		int mr = c - (i - c);
-
-		f[i] = (r >= i ? min(f[mr], r - i) : 0);
-		while (t[i + (f[i] + 1)] == t[i - (f[i] + 1)])
-			++f[i];
-
-		if (i + f[i] > r)
-			c = i, r = i + f[i];
-	}
 }
